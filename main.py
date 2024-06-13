e89af429c959372d7b6a19f5ef4a5b61353657cf
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-06-13 19:08:17 -0300

    acomode un poco el codigo

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def fusionarArchivosCSV(lista_archivos_entrada: list[str], archivo_salida: str) 
     publicado de la linea 144) y genera un nuevo archivo en memoria con todos los contenidos de los archivos
     recien mencionados. No devuelve nada.
     """
-    with open(archivo_salida, 'w') as salida:
-        for i,archivo in enumerate(lista_archivos_entrada):
+    with open(archivo_salida, "w") as salida:
+        for i, archivo in enumerate(lista_archivos_entrada):
             with open(archivo) as entrada:
-                #si el indice es 0, copie todo el archivo, sino que me saltee la primer linea
-                if(i == 0):  
+                # si el indice es 0, copie todo el archivo, sino que me saltee la primer linea
+                if i == 0:
                     salida.writelines(entrada.readlines())
                 else:
                     salida.writelines(entrada.readlines()[1:])
@@ -26,13 +26,17 @@ def obtenerAnios(nombre_archivo: str) -> list[int]:
     with open(nombre_archivo) as archivo:
         lineas = archivo.readlines()
         lista_anios = []
-        for i,linea in enumerate(lineas):
-            #obtengo el anio del archivo
-            anio = linea.split(",")[0].split('-')[0]
-            if(linea != '\n' and i > 0 and anio not in lista_anios):
+
+        for i, linea in enumerate(lineas):
+            # obtengo el anio del archivo
+            anio = linea.split(",")[0].split("-")[0]
+
+            if linea != "\n" and i > 0 and anio not in lista_anios:
                 lista_anios.append(anio)
-        #transformo en entero los anios de la lista agregada
+
+        # transformo en entero los anios de la lista agregada
         lista_anios = map(lambda x: int(x), lista_anios)
+
         return list(lista_anios)
 
 
@@ -52,25 +56,33 @@ def crearEstadisticasAnualDesdeArchivo(
 
     with open(nombre_archivo) as archivo:
         anios = obtenerAnios(nombre_archivo)
-        if (len(anios) == 0 or anio not in anios):
-            return None        
-        contar_provincias = {} 
+        
+        if len(anios) == 0 or anio not in anios:
+            return None
+        
+        contar_provincias = {}
         lineas = archivo.readlines()
+        
         for i, linea in enumerate(lineas):
-            if(linea != '\n' and i > 0):
+            if linea != "\n" and i > 0:
                 lista_lineas = linea.split(",")
                 provincia = lista_lineas[1]
-                
+
                 provincia_existente = contar_provincias.get(provincia)
-                
+
                 if provincia_existente:
                     contar_provincias[provincia] += 1
                 else:
                     contar_provincias[provincia] = 1
-                    
+
         print(contar_provincias)
 
-lista = ["./datos/datosVG2020.csv","./datos/datosVG2021.csv","./datos/datosVG2022.csv"]
-fusionarArchivosCSV(lista,"./datos/datos_filtrados.csv")
+
+lista = [
+    "./datos/datosVG2020.csv",
+    "./datos/datosVG2021.csv",
+    "./datos/datosVG2022.csv",
+]
+fusionarArchivosCSV(lista, "./datos/datos_filtrados.csv")
 obtenerAnios("./datos/datos_filtrados.csv")
 crearEstadisticasAnualDesdeArchivo("./datos/datos_filtrados.csv", 2022)
