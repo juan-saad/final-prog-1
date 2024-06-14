80660233cde7cc7e110400a5e2133b4c498bcadd
ulises2125 <ulisesresumenes@gmail.com>
2024-06-13 21:07:46 -0300

    semana 1 y 2 terminada

--- main.py
+++ main.py
@@ -53,43 +53,75 @@ def crearEstadisticasAnualDesdeArchivo(
         y promedio_edades: int
           que es el promedio de edades de las personas que llamaron a esa línea durante todo el año
     """
-
+    anios = obtenerAnios(nombre_archivo)
+    if len(anios) == 0 or anio not in anios:
+        return None
+    contar_provincias = {}
+    promedios_edades = 0
+    lista_edades = []
     with open(nombre_archivo) as archivo:
-        anios = obtenerAnios(nombre_archivo)
-        
-        if len(anios) == 0 or anio not in anios:
-            return None
-        
-        contar_provincias = {}
-        promedios_edades = 0
-        lineas = archivo.readlines()
-        lista_edades = []
-        for i, linea in enumerate(lineas):
-            if linea != "\n" and i > 0:
-                lista_lineas = linea.split(",")
-                provincia = lista_lineas[1]
-                edad = lista_lineas[3]
+        lector = csv.reader(archivo)
+        # Omitir el encabezado
+        next(lector, None) 
+        for fila in lector:
+            if fila and int(fila[0][:4]) == anio:
+                provincia = fila[1]
+                edad = fila[3]
                 provincia_existente = contar_provincias.get(provincia)
                 if provincia_existente:
                     contar_provincias[provincia] += 1
                 else:
                     contar_provincias[provincia] = 1
                 if(edad):
-                    print(i)
-                    int(edad)
-                    #lista_edades.append(edad)
-        #print(lista_edades,"edades")
+                    lista_edades.append(int(edad))
+        promedios_edades = sum(lista_edades) / len(lista_edades)
+        return contar_provincias,promedios_edades
 
 
-        #print(contar_provincias)
+"""
+La clase EstadisticasAnual se utilizará para mantener información sobre
+estadísticas de Situaciones de Violencia en Argentina en un año en particular
+"""
+
+class EstadisticasAnual:
+    def __init__(self, anio: int, cant_llamadas_por_provincia:dict[str,int], promedio_edad_llamantes: int):
+        self._anio = anio
+        self._cant_llamadas_por_provincia = cant_llamadas_por_provincia
+        self._promedio_edad_llamantes = promedio_edad_llamantes
+
+    def get_anio(self) -> int:
+        return self._anio
+
+    def get_cant_llamadas_por_provincia(self) -> dict[str,int]:
+        return self._cant_llamadas_por_provincia
+
+    def get_promedio_edad_llamantes(self) -> int:
+        return self._promedio_edad_llamantes
+
+    def __str__(self)->str:
+        return (
+            f"Año: {self._anio}\n"
+            f"Cantidad de llamadas por provincia: {self._cant_llamadas_por_provincia}\n"
+            f"Promedio de edad de los llamantes: {self._promedio_edad_llamantes}"
+        )
+
+def crearObjetosEstadisticasAnual(nombre_archivo:str) -> list[EstadisticasAnual]:
+    anios = obtenerAnios(nombre_archivo)
+    estadistica_anual = []
+    for anio in anios:
+        resultado = crearEstadisticasAnualDesdeArchivo(nombre_archivo,anio)
+        estadistica_anual.append(EstadisticasAnual(anio,resultado[0],resultado[1]))
+    return estadistica_anual
 
 
+
+#-------------------- Proceso principal ---------------------
 lista = [
     "./datos/datosVG2020.csv",
     "./datos/datosVG2021.csv",
     "./datos/datosVG2022.csv",
 ]
-
-fusionarArchivosCSV(lista, "./datos/datos_filtrados.csv")
-obtenerAnios("./datos/datos_filtrados.csv")
-crearEstadisticasAnualDesdeArchivo("./datos/datos_filtrados.csv", 2022)
+#fusionarArchivosCSV(lista, "./datos/datos_filtrados.csv")
+#obtenerAnios("./datos/datos_filtrados.csv")
+#crearEstadisticasAnualDesdeArchivo("./datos/datos_filtrados.csv", 2022)
+crearObjetosEstadisticasAnual("./datos/datos_filtrados.csv")
